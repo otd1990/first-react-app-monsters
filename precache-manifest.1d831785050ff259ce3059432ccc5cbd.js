4838d4fb530897f3dc2308303e97ccbf8fbf6b52
oliverDyer <odyer4@gmail.com>
2020-05-17 07:51:52 +0100

    Updates

---
+++ precache-manifest.1d831785050ff259ce3059432ccc5cbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a9976c6a211b7e519acee7326d082c",
+    "url": "/first-react-app-monsters/index.html"
+  },
+  {
+    "revision": "dc10e3d3b6783bccce08",
+    "url": "/first-react-app-monsters/static/css/main.db3bdc12.chunk.css"
+  },
+  {
+    "revision": "e0437b2a4c2c48b4faac",
+    "url": "/first-react-app-monsters/static/js/2.e85c222a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-app-monsters/static/js/2.e85c222a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc10e3d3b6783bccce08",
+    "url": "/first-react-app-monsters/static/js/main.78d8c2ed.chunk.js"
+  },
+  {
+    "revision": "774dee8300f1b68ce397",
+    "url": "/first-react-app-monsters/static/js/runtime-main.4e8d392f.js"
+  }
+]);
